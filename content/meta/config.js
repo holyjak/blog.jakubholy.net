fb8ed257af67fa73dfd3d7f3a4ead7d2610e99b9
Jakub Holy <jakub.holy@telia.no>
2018-12-12 11:02:01 +0100

    Replace Greg with me in site info

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  siteTitle: "HeroBlog - a GatsbyJS starter", // <title>
-  shortSiteTitle: "HeroBlog - GatsbyJS starter", // <title> ending for posts and pages
-  siteDescription: "HeroBlog is a GatsbyJS starter.",
-  siteUrl: "https://gatsby-starter-hero-blog.greglobinski.com",
+  siteTitle: "Holy on Dev", // <title>
+  shortSiteTitle: "Holy on Dev", // <title> ending for posts and pages
+  siteDescription: "Software development, productivity, and tea",
+  siteUrl: "https://blog.jakubholy.net",
   // pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
 
   /* author */
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "jakub holý",
+  authorTwitterAccount: "holyjak",
 
   /* info */
-  headerTitle: "greg lobinski",
-  headerSubTitle: "presents another one GatsbyJS starter",
+  headerTitle: "jakub holý",
+  headerSubTitle: "on software development, productivity, and tea",
 
   /* manifest.json */
-  manifestName: "HeroBlog - a GatsbyJS starter",
-  manifestShortName: "HeroBlog", // max 12 characters
+  manifestName: "Holy on Dev",
+  manifestShortName: "Holy on Dev", // max 12 characters
   manifestStartUrl: "/index.html",
   manifestBackgroundColor: "white",
   manifestThemeColor: "#666",
@@ -28,12 +28,12 @@ module.exports = {
   // Replace your email adress with md5-code.
   // Example https://www.gravatar.com/avatar/g.strainovic@gmail.com ->
   // gravatarImgMd5: "https://www.gravatar.com/avatar/1db853e4df386e8f699e4b35505dd8c6",
-  gravatarImgMd5: "",
+  gravatarImgMd5: "https://www.gravatar.com/avatar/32b26dbbf1f84656393a57a292c73728",
 
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/holyjak" },
+    { name: "twitter", url: "https://twitter.com/holyjak" }
+    //,{ name: "facebook", url: "http://facebook.com/XXXX" }
   ]
 };
--- content/parts/footnote.md
+++ content/parts/footnote.md
@@ -2,9 +2,6 @@
 title: footnote
 ---
 
-* it's a demo site of the [gatsby-starter-hero-blog](https://github.com/greglobinski/gatsby-starter-hero-blog)
-* built by [greg lobinski](https://www.greglobinski.com)
-* Check [Front-end web development with Greg](https://dev.greglobinski.com)
-* delivered by [Netlify](https://www.netlify.com/)
+* based on greg lobinski's [gatsby-starter-hero-blog](https://github.com/greglobinski/gatsby-starter-hero-blog), powered by [GatsbyJS](https://gatsbyjs.org/)
 * photos by [unsplash.com](https://unsplash.com)
 * graphic by [pixabay.com](https://pixabay.com)
