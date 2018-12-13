0eb0dde80ca0c6c3435e9a0ecdb446f54c494f1f
Jakub Holy <jakub.holy@telia.no>
2018-12-13 10:36:02 +0100

    TMP Menu: Disable search, contact until fixed

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -25,9 +25,9 @@ class Menu extends React.Component {
     this.items = [
       { to: "/", label: "Home", icon: FaHome },
       { to: "/category/", label: "Categories", icon: FaTag },
-      { to: "/search/", label: "Search", icon: FaSearch },
+      //{ to: "/search/", label: "Search", icon: FaSearch },
       ...pages,
-      { to: "/contact/", label: "Contact", icon: FaEnvelope }
+      //{ to: "/contact/", label: "Contact", icon: FaEnvelope }
     ];
 
     this.renderedItems = []; // will contain references to rendered DOM elements of menu
