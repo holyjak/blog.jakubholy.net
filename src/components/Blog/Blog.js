cb03fcf38a82c3685d549ee3b9a5434bebf17e49
Jakub Holy <jakub.holy@telia.no>
2019-03-16 01:05:52 +0100

    WIP Link to tumblr

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -5,23 +5,53 @@ import { Link } from "gatsby";
 
 import Item from "./Item";
 
+const TumblrFeed = props => (
+  <React.Fragment>
+    <li key="tumblr">
+      <aside>
+        <h1>Recommended links feed</h1>
+        At <a href="https://holyjak.tumblr.com/">holyjak.tumblr.com</a> you get a feed of the most
+        interesting articles, libraries, and other links I have found lately.
+      </aside>
+    </li>
+    <style jsx>{`
+      li {
+        border: 1px solid transparent;
+        background-color: #dddbda;
+        padding: 10px;
+        box-shadow: 3px 3px 3px 3px #969492;
+        margin-top: 2em;
+      }
+    `}</style>
+  </React.Fragment>
+);
+
 const Blog = props => {
   const { posts, theme, pagination } = props;
   const { previousPagePath, nextPagePath, pageNumber, numberOfPages } = pagination;
   const numberOfNextPages = numberOfPages - pageNumber;
   const numberOfPreviousPages = pageNumber - 1;
+  const firstPage = pageNumber === 1;
+
+  const items = posts.map(post => {
+    const {
+      node,
+      node: { slug }
+    } = post;
+    return <Item key={slug} post={node} theme={theme} />;
+  });
+
+  if (firstPage) {
+    const elm = <TumblrFeed key="tumblr" />;
+    const pos = items.length > 0 ? 1 : 0;
+    items.splice(pos, 0, elm);
+  }
 
   return (
     <React.Fragment>
       <main className="main">
         <ul>
-          {posts.map(post => {
-            const {
-              node,
-              node: { slug }
-            } = post;
-            return <Item key={slug} post={node} theme={theme} />;
-          })}
+          {items}
           <li>
             {previousPagePath ? (
               <Link id="link-prev-page" to={previousPagePath}>
