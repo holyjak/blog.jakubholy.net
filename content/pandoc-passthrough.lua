1e171594934082aa6cd8820b9cef5f2139dd154d
Jakub Holy <jakub.holy@telia.no>
2019-12-04 09:12:59 +0100

    Fix <!--more--> to not create <p>

--- content/pandoc-passthrough.lua
+++ content/pandoc-passthrough.lua
@@ -3,6 +3,7 @@ function RawInline(el)
 end
 function RawBlock(el)
   if el.format == 'html' and el.text:match("<!--") then
-    return pandoc.RawBlock('asciidoc', '\n+++' .. el.text .. '+++\n\n')
+	  -- return pandoc.RawBlock('asciidoc', '\n+++' .. el.text .. '+++\n\n')
+    return pandoc.RawBlock('asciidoc', '\n++++\n' .. el.text .. '\n++++\n\n')
   end
 end
