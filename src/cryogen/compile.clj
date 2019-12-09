ac9cd87728a0a148ee29e4893b7a1a55ee69848a
Jakub Holy <jakub.holy@telia.no>
2019-12-09 08:54:34 +0100

    Styling of /contact

--- src/cryogen/compile.clj
+++ src/cryogen/compile.clj
@@ -4,7 +4,5 @@
 (defn compile-site []
   (compile-assets-timed
     {:update-article (fn [{:keys [slug] :as article}]
-                       (when slug (println "Replacing URI with slug" (:uri article) "->" slug)) ;; FIXME rm
-                       ;(swap! cryogen.core/*a conj article)
                        (cond-> article
                                slug (assoc :uri (str "/" slug "/"))))}))
