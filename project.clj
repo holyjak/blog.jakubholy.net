b064460de7e9b4633d9a7e7f7138d4ca40d16208
Jakub Holy <jakubholy@jakubholy.net>
2021-10-04 21:39:28 +0200

    dev: refresh browser page on change

--- project.clj
+++ project.clj
@@ -4,15 +4,18 @@
             :license {:name "Eclipse Public License"
                       :url "http://www.eclipse.org/legal/epl-v10.html"}
             :dependencies [[org.clojure/clojure "1.10.1"]
-                           [ring/ring-devel "1.7.1"]
-                           [compojure "1.6.1"]
+                           [ring/ring-devel "1.8.2"]
+                           [compojure "1.6.2"]
                            [ring-server "0.5.0"]
                            [cryogen-asciidoc "0.3.3"]
-                           [cryogen-core "0.3.2"]
+                           [cryogen-core "0.4.0"]
+                           [cryogen-asciidoc "0.3.3"]
                            ;; TMP data.xml to parse old blog posts:
                            #_[org.clojure/data.json "0.2.7"]]
             ;:repositories [["local" "file:///Users/holyjak/.m2"]]
             :plugins [[lein-ring "0.12.5"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
-                   :handler cryogen.server/handler})
+                   :handler cryogen.server/handler}
+            :aliases {"serve"      ["run" "-m" "cryogen.server"]
+                      "serve-fast" ["run" "-m" "cryogen.server" "fast"]})
--- src/cryogen/server.clj
+++ src/cryogen/server.clj
@@ -1,25 +1,30 @@
 (ns cryogen.server
-  (:require [cryogen.compile :as my.compile]
-            [compojure.core :refer [GET defroutes]]
-            [compojure.route :as route]
-            [ring.util.response :refer [redirect file-response]]
-            [ring.util.codec :refer [url-decode]]
-            [ring.server.standalone :as ring-server]
-            [cryogen-core.watcher :refer [start-watcher! start-watcher-for-changes!]]
-            [cryogen-core.plugins :refer [load-plugins]]
-            [cryogen-core.compiler :refer [compile-assets-timed]]
-            [cryogen-core.config :refer [resolve-config]]
-            [cryogen-core.io :refer [path]]
-            [clojure.string]))
+  (:require
+    [cryogen.compile :as my.compile]
+    [compojure.core :refer [GET defroutes]]
+    [compojure.route :as route]
+    [ring.util.response :refer [redirect file-response]]
+    [ring.util.codec :refer [url-decode]]
+    [ring.server.standalone :as ring-server]
+    [cryogen-core.watcher :refer [start-watcher! start-watcher-for-changes!]]
+    [cryogen-core.plugins :refer [load-plugins]]
+    [cryogen-core.compiler :refer [compile-assets-timed]]
+    [cryogen-core.config :refer [resolve-config]]
+    [cryogen-core.io :refer [path]]
+    [clojure.string :as string])
+  (:import (java.io File)))
+
+(def resolved-config (delay (resolve-config)))
 
 (def extra-config-dev
+  "Add dev-time configuration overrides here, such as `:hide-future-posts? false`"
   (merge my.compile/extra-config
          {:hide-future-posts? false}))
 
 (defn init [fast?]
   (load-plugins)
   (compile-assets-timed extra-config-dev)
-  (let [ignored-files (-> (resolve-config) :ignored-files)]
+  (let [ignored-files (-> @resolved-config :ignored-files)]
     (run!
       #(if fast?
          (start-watcher-for-changes! % ignored-files compile-assets-timed extra-config-dev)
@@ -29,19 +34,34 @@
 (defn wrap-subdirectories
   [handler]
   (fn [request]
-    (let [{:keys [clean-urls blog-prefix public-dest]} (resolve-config)
+    (let [{:keys [clean-urls blog-prefix public-dest]} @resolved-config
           req-uri (.substring (url-decode (:uri request)) 1)
-          res-path (condp = clean-urls
-                     :trailing-slash (path req-uri "index.html")
-                     :no-trailing-slash (if (or (= req-uri "")
-                                                (= req-uri "/")
-                                                (= req-uri
-                                                   (.substring blog-prefix 1)))
-                                          (path req-uri "index.html")
-                                          (path (str req-uri ".html")))
-                     :dirty (path (str req-uri ".html")))]
-      (or (file-response res-path {:root public-dest})
-          (handler request)))))
+          res-path (if (or (.endsWith req-uri "/")
+                           (.endsWith req-uri ".html")
+                           (-> (string/split req-uri #"/")
+                               last
+                               (string/includes? ".")
+                               not))
+                     (condp = clean-urls
+                       :trailing-slash (path req-uri "index.html")
+                       :no-trailing-slash (if (or (= req-uri "")
+                                                  (= req-uri "/")
+                                                  (= req-uri
+                                                     (if (string/blank? blog-prefix)
+                                                       blog-prefix
+                                                       (.substring blog-prefix 1))))
+                                            (path req-uri "index.html")
+                                            (path (str req-uri ".html")))
+                       :dirty (path (str req-uri ".html")))
+                     req-uri)]
+        (or (let [rsp (file-response res-path {:root public-dest})
+                  body (:body rsp)]
+              (cond-> rsp
+                      (and body
+                           (instance? File body)
+                           (string/ends-with? (.getName body) ".html"))
+                      (assoc-in [:headers "Content-Type"] "text/html")))
+            (handler request)))))
 
 (defroutes routes
   (GET "/" [] (redirect (let [config (resolve-config)]
@@ -55,11 +75,22 @@
 
 (defn serve
   "Entrypoint for running via tools-deps (clojure)"
-  [{:keys [fast] :as opts}]
+  [{:keys [fast join?] :as opts}]
   (ring-server/serve
     handler
-    (merge {:init (partial init fast)} opts)))
+    (merge
+      {:join? (if (some? join?) join? true)
+       :init (partial init fast)
+       :open-browser? true
+       :auto-refresh? fast ; w/o fast it would often try to reload the page before it has been fully compiled
+       :refresh-paths [(:public-dest @resolved-config)]}
+      opts)))
+
+(defn -main [& args]
+  (serve {:port 3000, :fast ((set args) "fast")}))
 
 (comment
-  (serve nil)
-  nil)
+  (def srv (serve {:join? false, :fast true}))
+  (.stop srv)
+
+  ,)
